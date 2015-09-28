8c567a067d4ec1b4cbf990c40ea44ae668dde528
ishaanj <ishjain321@gmail.com>
2015-09-28 21:16:38 +0530

    Client - Server communication finished

--- src/communication/Client.java
+++ src/communication/Client.java
@@ -1,7 +1,97 @@
 package communication;
 
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import cryptography.*;
+
 public class Client {
 
-	// Make sure to take file size as an input
+	static Socket me;
+	static String serverAddr = "localhost";
+	static int port;
+	static String fileName,opPath = "src/testFileOutput/test1op.txt";
+	static byte[] freeFile;
+	static byte[][] chunkyfileByte, unchunked;
+	static ArrayList<Future<byte[]>> futureByte;
+	static AES aes;
+	static FileManager fm;
 	
+	public static void main(String[] args)throws IOException, InterruptedException, ExecutionException {
+		
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	//IP, port input
+		//System.out.println("Enter the ip address/ hostname to connect on");
+		//serverAddr = br.readLine();
+	System.out.println("Enter port number");
+	port = Integer.parseInt(br.readLine());
+	
+	//take file input here
+	System.out.println("Enter filename: ");
+	fileName = br.readLine();
+			
+	try
+    {
+	   //connection
+       System.out.println("Connecting to " + serverAddr +
+		 " on port " + port);
+       me = new Socket(serverAddr, port);
+       System.out.println("Just connected to " 
+		 + me.getRemoteSocketAddress());
+       
+       //server to client keystring
+       InputStream inFromServer = me.getInputStream();
+       DataInputStream in =
+                      new DataInputStream(inFromServer);
+       String keystring = in.readUTF();
+       System.out.println("Recieved key: " + keystring);
+       aes = new AES(AES.stringToKey(keystring));
+       
+       
+       //client to server filename
+       OutputStream outToServer = me.getOutputStream();
+       DataOutputStream out = new DataOutputStream(outToServer);
+       out.writeUTF("Key recieved, filename incoming ");
+       out.flush();
+       System.out.println("Sending filename");
+       out.writeUTF(fileName);
+       
+       //server file response
+       if(in.readUTF().equals("found")){
+    	   System.out.println("File found, proceeding with transfer");
+       }
+       else{
+    	   System.out.println("File not found, closing connection");
+    	   me.close();
+       }
+       
+       //read byte chunks
+       int fblen = in.readInt();
+       int csize = in.readInt();
+       for(int i = 0 ; i < fblen ; i++){
+    	   in.readFully(chunkyfileByte[i],0,csize);
+       }
+       
+       //CryptoManager
+       futureByte = CryptoManager.getExecutor(aes).decryptFile(chunkyfileByte);
+       
+       //Decrypt arraylist
+       for(int i = 0 ; i < futureByte.size() ; i++){
+    	   unchunked[i] = futureByte.get(i).get();
+       }
+       freeFile = fm.dechunkify(unchunked);
+       
+       //write file
+       fm.writeFile(freeFile, opPath);
+       //close connection
+       me.close();
+    }catch(IOException e)
+    {
+       e.printStackTrace();
+    }
+	}
 }
--- src/communication/Server.java
+++ src/communication/Server.java
@@ -1,7 +1,102 @@
 package communication;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+import cryptography.*;
+
 public class Server {
 
-	// Make sure to send file size as in output
+	static ServerSocket me;
+	static Socket him;
+	static int port, timeout = 100000;
+	static String fileName, basePath = "src/testFileInput/";
+	static File file;
+	static byte[] fileByte;
+	static byte[][] chunkyFileByte;
+	static ArrayList<Future<byte[]>> futureByte;
+	static AES aes;
+	static FileManager fm;
+
+	public static void main(String[] args)throws IOException, InterruptedException, ExecutionException {
 	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter the port number to listen on");
+		port = Integer.parseInt(br.readLine());
+		
+		//create server and set timeout
+		me = new ServerSocket(port);
+		me.setSoTimeout(timeout);
+		
+		//create AES instance
+		aes = new AES();
+		String keystring = aes.keyToString();
+		
+		while(true)
+		{
+	         try
+	         {
+	        	//connection
+	            System.out.println("Waiting for client on port: " +
+	            me.getLocalPort() + "...");
+	            Socket him = me.accept();
+	            System.out.println("Client connected from: "
+	                  + him.getRemoteSocketAddress());
+	            
+	            //server to client keystring
+	            DataOutputStream out =
+	                 new DataOutputStream(him.getOutputStream());
+	            out.writeUTF(keystring);
+	            
+	            //client to server filename
+	            DataInputStream in =
+	                  new DataInputStream(him.getInputStream());
+	            System.out.println(in.readUTF());
+	            fileName = in.readUTF();
+	            
+	            //file from server to client
+	            file = new File(basePath+fileName);
+	           	if(file.exists()){
+	           		System.out.println("Requested File found");
+	           		out.writeUTF("found");
+	           	}
+	           	else{
+	           		System.out.println("Requested File not found");
+	           		out.writeUTF("!found");
+	           		System.out.println("Closing connection");
+	           		him.close();
+	           	}
+	           	
+	           	//FileManager 
+	           	fm = new FileManager();
+	           	fileByte = fm.readFile(basePath+fileName);
+	           	System.out.println("Reading file and chunkifying");
+	           	chunkyFileByte = fm.chunkify(fileByte);
+	           	
+	           	//CryptoManager
+	           	futureByte = CryptoManager.getExecutor(aes).encryptFile(chunkyFileByte);
+	           	out.writeInt(futureByte.size());
+	           	out.writeInt(fm.chunkSize);
+	           	System.out.println("Sending File chunks");
+	           	for(int i = 0 ; i < futureByte.size() - 1 ; i++){
+	           		out.write(futureByte.get(i).get(),0,fm.chunkSize);
+	           	}
+	           	
+	            //close connection
+	            him.close();
+	         }catch(SocketTimeoutException s)
+	         {
+	            System.out.println("Socket timed out!");
+	            break;
+	         }catch(IOException e)
+	         {
+	            e.printStackTrace();
+	            break;
+	         }
+	      }
+		
+	}
 }
